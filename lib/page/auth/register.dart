67d168392a14770030dd8c76b66170db4b1fab67
faynald <72617106+faynald@users.noreply.github.com>
2020-10-31 19:04:36 +0700

    beta version of login/register

--- lib/page/auth/register.dart
+++ lib/page/auth/register.dart
@@ -1,1 +1,24 @@
-//import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: null);
+              })
+        ],
+        title: Text('Register'),
+      ),
+    );
+  }
+}
