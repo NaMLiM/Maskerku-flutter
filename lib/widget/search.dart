d5dc3c1dc25ff1244876379724d158758f0b2dd5
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-10-31 17:47:23 +0700

    add search class (Test)

---
+++ lib/widget/search.dart
@@ -0,0 +1,37 @@
+//Test Class
+
+import 'package:flutter/material.dart';
+
+class Search extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pop(context);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(''),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {}
+}
