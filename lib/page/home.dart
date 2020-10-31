554e683ae99d16ebe7ad6289784d8a23c7d6366e
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-10-31 17:46:40 +0700

    add search button (wip feature)

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maskerku/widget/drawer.dart';
+import 'package:maskerku/widget/search.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,7 +12,14 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Maskerku"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: null);
+              })
+        ],
+        title: Text('Maskerku'),
       ),
       drawer: MaskDrawer(),
     );
