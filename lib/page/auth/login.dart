7e36ab6944f55f4500f66539fb87951afc31e6c8
faynald <72617106+faynald@users.noreply.github.com>
2020-11-01 18:11:21 +0700

    UI improvement on login page

--- lib/page/auth/login.dart
+++ lib/page/auth/login.dart
@@ -6,18 +6,80 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  String _username = '';
+  String _password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                showSearch(context: context, delegate: null);
-              })
+      backgroundColor: Colors.purple,
+      body: Column(
+        children: <Widget>[
+          Container(//logo
+            margin: EdgeInsets.only(top: 30.0),
+            child: Image.asset('assets/icon-bmkg.png'),
+            //Image.asset('assets/icon-bmkg.png'),
+          ),
+          Container(//text login
+            margin: EdgeInsets.only(top: 10.0),
+            alignment: Alignment.center,
+            child: Text(
+              'Login',
+              style: TextStyle(color: Colors.white,fontSize: 32),
+            ),
+          ),
+          Container(//textfield
+            margin: EdgeInsets.all(30.0),
+            child: Column(
+              children: <Widget>[
+                Column(//username
+                  children: [
+                    TextField(
+                      onChanged: (String value){
+                        setState(() {
+                          _username = value;
+                        });
+                      },
+                      decoration: new InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white70)
+                        ),
+                        hintText: 'Enter Your Username',
+                        labelText: 'Username',
+                        labelStyle: new TextStyle(
+                          color: Colors.white70,
+                          fontWeight: FontWeight.bold
+                        )
+                      ),
+                    )
+                  ],
+                ),
+                Column(//password
+                  children: [
+                    SizedBox(height: 30,),
+                    TextField(
+                      onChanged: (String value){
+                        setState(() {
+                          _password = value;
+                        });
+                      },
+                      decoration: new InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white70)
+                        ),
+                        hintText: 'Enter Your Password',
+                        labelText: 'Password',
+                        labelStyle: new TextStyle(
+                          color: Colors.white70,
+                          fontWeight: FontWeight.bold
+                        )
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
         ],
-        title: Text('Login'),
       ),
     );
   }
