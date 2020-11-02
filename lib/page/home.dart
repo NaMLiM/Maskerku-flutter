5d047ecb705a51fd9f3573bd815603b98a1434f1
faynald <72617106+faynald@users.noreply.github.com>
2020-11-02 23:20:31 +0700

    added some features

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -22,6 +22,69 @@ class _HomeState extends State<Home> {
         title: Text('Maskerku'),
       ),
       drawer: DrawerCustom(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                children: [
+                  Row( //Create your mask & Choose your design
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //spacing: 8.0, // gap between adjacent chips
+                    //runSpacing: 4.0, // gap between lines
+                    children: [
+                      FlatButton(
+                          onPressed: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Home()),
+                            );
+                          }, 
+                          child: Image.asset('assets/create-your-mask.png',width: 150)
+                        ),
+                      FlatButton(
+                          onPressed: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Home()),
+                            );
+                          }, 
+                          child: Image.asset('assets/choose-your-design.png',width: 150)
+                        ),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Row( //text & Button
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Terpopuler :",
+                        style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),
+                      ),
+                      FlatButton(
+                        onPressed: (){
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Home()),
+                            );
+                        }, 
+                        child: Row(
+                          children: [
+                            Text("More"),
+                            Icon(Icons.keyboard_arrow_right)
+                          ],
+                        )
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            //SizedBox(height: 20),
+            
+          ],
+        )
+      ),
     );
   }
 }
