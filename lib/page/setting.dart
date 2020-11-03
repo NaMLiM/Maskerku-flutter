1d5bbb84e4cb01b3a9d2a3c6e02554e9c8f39a73
faynald <72617106+faynald@users.noreply.github.com>
2020-11-03 20:35:35 +0700

    added some features

--- lib/page/setting.dart
+++ lib/page/setting.dart
@@ -6,27 +6,61 @@ class Setting extends StatefulWidget{
 }
 
 class _SettingState extends State<Setting>{
+  bool vibrateSwitched = true;
+  bool notifSwitched = true;
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                showSearch(context: context, delegate: null);
-              })
-        ],
         title: Text('Setting'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              child: Text('asd'),
-            )
-          ],
-        )
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children : [
+                  Text('Vibrate',style: TextStyle(fontSize: 20),),
+                  Switch(value: vibrateSwitched, onChanged: (value){
+                    setState(() {
+                      vibrateSwitched=value;
+                      print(vibrateSwitched);
+                    });
+                  },)
+                ]
+              ),
+              Divider(
+                //Line
+                height: 5.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children : [
+                  Text('Notification',style: TextStyle(fontSize: 20),),
+                  Switch(value: notifSwitched, onChanged: (value){
+                    setState(() {
+                      notifSwitched=value;
+                      print(notifSwitched);
+                    });
+                  },)
+                ]
+              ),
+              Divider(
+                //Line
+                height: 5.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Version', style: TextStyle(fontSize: 20),),
+                  Text('V0.1 (Beta)', style: TextStyle(color: Colors.black54),)
+                ],
+              )
+            ],
+          )
+        ),
       ),
     );
   }
