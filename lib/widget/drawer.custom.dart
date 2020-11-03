031466a617356d71f47bce1a138428691ee178d3
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-11-03 20:00:12 +0700

    add state Change to drawer

--- lib/widget/drawer.custom.dart
+++ lib/widget/drawer.custom.dart
@@ -14,76 +14,96 @@ class DrawerCustom extends StatefulWidget {
 }
 
 class _DrawerState extends State<DrawerCustom> {
+  bool showUserDetails = false;
+
+  Widget _drawerList() {
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        ListTile(
+          //Home
+          leading: Icon(Icons.home),
+          title: Text('Home'),
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        ListTile(
+          //Setting
+          leading: Icon(Icons.settings),
+          title: Text('Setting'),
+          onTap: () {
+            Navigator.of(context).pop();
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Setting()));
+          },
+        ),
+        ListTile(
+          //About Us
+          leading: Icon(Icons.account_box),
+          title: Text('About Us'),
+          onTap: () {
+            Navigator.of(context).pop();
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => About()));
+          },
+        ),
+        Divider(
+          //Line
+          height: 5.0,
+        ),
+        ListTile(
+          //Login
+          leading: Icon(Icons.login),
+          title: Text('Login'),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Login()),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _userDetails() {
+    return Container(
+      color: Colors.lightBlue,
+      child: ListView(children: [
+        ListTile(
+          title: Text("User Details"),
+          leading: Icon(Icons.info_outline),
+        )
+      ]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: Text("Anonim"),
-            accountEmail: Text("andicol@gmail.com"),
-            currentAccountPicture: CircleAvatar(
-              backgroundColor:
-                  Theme.of(context).platform == TargetPlatform.android
-                      ? Colors.blue
-                      : Colors.white,
-              child: Text(
-                "A",
-                style: TextStyle(fontSize: 40.0),
-              ),
+      child: Column(children: [
+        UserAccountsDrawerHeader(
+          accountName: Text("Anonim"),
+          accountEmail: Text("andicol@gmail.com"),
+          currentAccountPicture: CircleAvatar(
+            backgroundColor:
+                Theme.of(context).platform == TargetPlatform.android
+                    ? Colors.blue
+                    : Colors.white,
+            child: Text(
+              "A",
+              style: TextStyle(fontSize: 40.0),
             ),
-            onDetailsPressed: () {
-              Navigator.of(context).pop();
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Profile()));
-            },
           ),
-          ListTile(
-            //Home
-            leading: Icon(Icons.home),
-            title: Text('Home'),
-            onTap: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          ListTile(
-            //Setting
-            leading: Icon(Icons.settings),
-            title: Text('Setting'),
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Setting()));
-            },
-          ),
-          ListTile(
-            //About Us
-            leading: Icon(Icons.account_box),
-            title: Text('About Us'),
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => About()));
-            },
-          ),
-          Divider(
-            //Line
-            height: 5.0,
-          ),
-          ListTile(
-            //Login
-            leading: Icon(Icons.login),
-            title: Text('Login'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Login()),
-              );
-            },
-          ),
-        ],
-      ),
+          onDetailsPressed: () {
+            setState(() {
+              showUserDetails = !showUserDetails;
+            });
+          },
+        ),
+        Expanded(child: showUserDetails ? _userDetails() : _drawerList())
+      ]),
     );
   }
 }
