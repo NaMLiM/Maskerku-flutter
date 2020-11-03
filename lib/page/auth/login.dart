e520eb124cddaa332a7752237b69bc5c1685068d
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-11-03 17:31:16 +0700

    fix code styling

--- lib/page/auth/login.dart
+++ lib/page/auth/login.dart
@@ -16,156 +16,157 @@ class _LoginState extends State<Login> {
       _obscureText = !_obscureText;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       body: Container(
-        decoration: BoxDecoration(//gradient background color
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: <Color>[
-                  '262995'.toColor(),
-                  Colors.black
-                ],
-              ),
+        decoration: BoxDecoration(
+          //gradient background color
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: <Color>['262995'.toColor(), Colors.black],
+          ),
+        ),
+        child: Column(children: [
+          Container(
+            //logo
+            margin: EdgeInsets.only(top: 30.0),
+            child: Image.asset(
+              'assets/icon-bmkg.png',
+              height: 150,
             ),
-        child: Column(
-          children: [
-            Container(//logo
-              margin: EdgeInsets.only(top: 30.0),
-              child: Image.asset(
-                'assets/icon-bmkg.png',
-                  height: 150,
+          ),
+          Container(
+            //all here, logo excluded
+            margin: EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.max,
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  //login text
+                  children: [
+                    Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 32),
+                    ),
+                  ],
                 ),
-            ),
-            Container(//all here, logo excluded
-              margin: EdgeInsets.symmetric(horizontal: 40.0, vertical:10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.max,
-                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(//login text
-                    children: [
-                      Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white,fontSize: 32),
-                      ),
-                    ],
-                  ),
-                  Column(//username
-                    children: [
-                      SizedBox(height: 35.0),
-                      TextField(
-                        onChanged: (String value){
-                          setState(() {
-                            //_username = value;
-                          });
-                        },
-                        style: TextStyle(color: Colors.white),
-                        decoration: new InputDecoration(
+                Column(
+                  //username
+                  children: [
+                    SizedBox(height: 35.0),
+                    TextField(
+                      onChanged: (String value) {
+                        setState(() {
+                          //_username = value;
+                        });
+                      },
+                      style: TextStyle(color: Colors.white),
+                      decoration: new InputDecoration(
                           enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white70)
-                          ),
+                              borderSide: BorderSide(color: Colors.white70)),
                           prefixIcon: Icon(Icons.people, color: Colors.white),
                           hintText: 'Enter Your Username',
                           hintStyle: new TextStyle(color: Colors.white38),
                           labelText: 'Username',
                           labelStyle: new TextStyle(
-                            color: Colors.white70,
-                            fontWeight: FontWeight.bold
-                          )
+                              color: Colors.white70,
+                              fontWeight: FontWeight.bold)),
+                    )
+                  ],
+                ),
+                Column(
+                  //password
+                  children: [
+                    SizedBox(height: 30),
+                    TextFormField(
+                      onChanged: (String value) {
+                        setState(() {
+                          //_password = value;
+                        });
+                      },
+                      style: TextStyle(color: Colors.white),
+                      decoration: new InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white70)),
+                        prefixIcon: Icon(Icons.lock, color: Colors.white),
+                        hintText: 'Enter Your Password',
+                        hintStyle: new TextStyle(color: Colors.white38),
+                        labelText: 'Password',
+                        labelStyle: new TextStyle(
+                            color: Colors.white70, fontWeight: FontWeight.bold),
+                        suffixIcon: IconButton(
+                          icon: Icon(_obscureText
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                          onPressed: _togglePasswordStatus,
+                          color: Colors.white,
                         ),
-                      )
-                    ],
-                  ),
-                  Column(//password
-                    children: [
-                      SizedBox(height: 30),
-                      TextFormField(
-                        onChanged: (String value){
-                          setState(() {
-                            //_password = value;
-                          });
-                        },
-                        style: TextStyle(color: Colors.white),
-                        decoration: new InputDecoration(
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white70)
-                          ),
-                          prefixIcon: Icon(Icons.lock, color: Colors.white),
-                          hintText: 'Enter Your Password',
-                          hintStyle: new TextStyle(color: Colors.white38),
-                          labelText: 'Password',
-                          labelStyle: new TextStyle(
-                            color: Colors.white70,
-                            fontWeight: FontWeight.bold
-                          ),
-                          suffixIcon:  IconButton(
-                            icon:Icon(_obscureText ? Icons.visibility_off:Icons.visibility),
-                            onPressed: _togglePasswordStatus,
-                            color: Colors.white,
-                            ),
-                        ),
-                        validator: (val) => val.length < 6 ? 'Password too short.' : null,
-                        //onSaved: (val) => _password = val,
-                        obscureText: _obscureText,
-                      )
-                    ],
-                  ),
-                  Column(//button login
-                    children: [
-                      SizedBox(height: 40),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: FlatButton(
-                          onPressed: (){
+                      ),
+                      validator: (val) =>
+                          val.length < 6 ? 'Password too short.' : null,
+                      //onSaved: (val) => _password = val,
+                      obscureText: _obscureText,
+                    )
+                  ],
+                ),
+                Column(
+                  //button login
+                  children: [
+                    SizedBox(height: 40),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: FlatButton(
+                          onPressed: () {
                             Navigator.push(
                               context,
                               MaterialPageRoute(builder: (context) => Home()),
                             );
-                          }, 
-                          child: Text('Login',style: TextStyle(fontSize: 20,color: Colors.white),),
+                          },
+                          child: Text(
+                            'Login',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
                           color: Colors.blue,
                           shape: RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(8.0)
-                            )
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column( //text register
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(height: 20.0),
-                      Text(
-                        "Don't have an account?",
-                        style: TextStyle(fontSize: 16, color: Colors.white70)
-                      ),
-                      InkWell(
-                        onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => Register()),
-                            );
-                        },
-                        child: new Text(
-                          "Register",
-                          style: TextStyle(
+                              borderRadius: new BorderRadius.circular(8.0))),
+                    ),
+                  ],
+                ),
+                Column(
+                  //text register
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(height: 20.0),
+                    Text("Don't have an account?",
+                        style: TextStyle(fontSize: 16, color: Colors.white70)),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Register()),
+                        );
+                      },
+                      child: new Text(
+                        "Register",
+                        style: TextStyle(
                             decoration: TextDecoration.underline,
-                            fontSize: 18, color: Colors.white),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
+                            fontSize: 18,
+                            color: Colors.white),
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
-          ]
-        ),
+          ),
+        ]),
       ),
     );
   }
