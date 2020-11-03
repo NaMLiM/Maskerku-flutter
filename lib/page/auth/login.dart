3a08a6310ebebef5bb83ab924b9a0f9ee8e58a3e
faynald <72617106+faynald@users.noreply.github.com>
2020-11-03 19:17:50 +0700

    extension changed

--- lib/page/auth/login.dart
+++ lib/page/auth/login.dart
@@ -172,14 +172,4 @@ class _LoginState extends State<Login> {
   }
 }
 
-extension ColorExtension on String {
-  toColor() {
-    var hexColor = this.replaceAll("#", "");
-    if (hexColor.length == 6) {
-      hexColor = "FF" + hexColor;
-    }
-    if (hexColor.length == 8) {
-      return Color(int.parse("0x$hexColor"));
-    }
-  }
-}
+
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -91,3 +91,15 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
+extension ColorExtension on String {
+  toColor() {
+    var hexColor = this.replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    }
+  }
+}
