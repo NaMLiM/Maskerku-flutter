6987e98df3cdf769b2d4a92a410975d5479470c9
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-11-04 21:39:52 +0700

    wip about page

--- lib/page/about.dart
+++ lib/page/about.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class About extends StatefulWidget{
+class About extends StatelessWidget {
   @override
-    _AboutState createState() => _AboutState();
-}
-
-class _AboutState extends State<About>{
-  @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('About Us'),
@@ -16,77 +11,115 @@ class _AboutState extends State<About>{
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
-            child: Container(
               child: Container(
-                padding: EdgeInsets.symmetric(vertical: 15),
-                margin: EdgeInsets.symmetric(vertical: 20,horizontal: 120),
-                child: Column(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: ClipRRect(
-                        child: Image.asset('assets/profile-anam.jpg'),
-                        borderRadius: BorderRadius.circular(50.0),
-                      )
-                    ),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text('Role Here',style: TextStyle(fontWeight: FontWeight.bold),)
-                        ],
-                      ),
-                    ),
-                    Expanded(child: Text('Name here'),)
-                  ],
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black26,width: 1),
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  color: const Color(0xFFEDEDED),
-                  )
-                ),
-              )
-            ),
-          Expanded(
-          child: Container(
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              margin: EdgeInsets.symmetric(vertical: 20,horizontal: 120),
-              child: Column(
-                children: [
-                  Expanded(
+            padding: EdgeInsets.symmetric(vertical: 15),
+            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 120),
+            child: Column(
+              children: [
+                Expanded(
                     flex: 3,
                     child: ClipRRect(
                       child: Image.asset('assets/profile-anam.jpg'),
                       borderRadius: BorderRadius.circular(50.0),
-                    )
+                    )),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Role Here',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ],
                   ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text('Role Here',style: TextStyle(fontWeight: FontWeight.bold),)
-                      ],
-                    ),
-                  ),
-                  Expanded(child: Text('Name here'),)
-                ],
-              ),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black26,width: 1),
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: const Color(0xFFEDEDED),
+                ),
+                Expanded(
+                  child: Text('Name here'),
                 )
-              ),
-            )
-          ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black26, width: 1),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: const Color(0xFFEDEDED),
+            ),
+          )),
           Expanded(
-            child: Container(
-              color: Colors.blue,
-              child: Text('Test 1'),
-            )
-          ),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 120),
+                  child: Column(
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: ClipRRect(
+                            child: Image.asset('assets/profile-anam.jpg'),
+                            borderRadius: BorderRadius.circular(50.0),
+                          )),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Role Here',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Text('Name here'),
+                      )
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black26, width: 1),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: const Color(0xFFEDEDED),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 120),
+                  child: Column(
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: ClipRRect(
+                            child: Image.asset('assets/profile-anam.jpg'),
+                            borderRadius: BorderRadius.circular(50.0),
+                          )),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Role Here',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Text('Name here'),
+                      )
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black26, width: 1),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: const Color(0xFFEDEDED),
+                  ),
+                )
+              ])),
+          Expanded(
+              child: Container(
+            color: Colors.blue,
+            child: Text('Test 1'),
+          )),
         ],
       ),
     );
