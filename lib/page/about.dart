7f7d01b7861ec008e90b001834462f13a96df850
faynald <72617106+faynald@users.noreply.github.com>
2020-11-04 21:55:36 +0700

    fix bugs

--- lib/page/about.dart
+++ lib/page/about.dart
@@ -48,72 +48,72 @@ class About extends StatelessWidget {
               child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 120),
-                  child: Column(
-                    children: [
-                      Expanded(
-                          flex: 3,
-                          child: ClipRRect(
-                            child: Image.asset('assets/profile-anam.jpg'),
-                            borderRadius: BorderRadius.circular(50.0),
-                          )),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              'Role Here',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            )
-                          ],
-                        ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      child: Column(
+                        children: [
+                          Expanded(
+                              flex: 3,
+                              child: ClipRRect(
+                                child: Image.asset('assets/profile-anam.jpg'),
+                                borderRadius: BorderRadius.circular(50.0),
+                              )),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                  'Role Here',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Text('Name here'),
+                          )
+                        ],
                       ),
-                      Expanded(
-                        child: Text('Name here'),
-                      )
-                    ],
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black26, width: 1),
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    color: const Color(0xFFEDEDED),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 120),
-                  child: Column(
-                    children: [
-                      Expanded(
-                          flex: 3,
-                          child: ClipRRect(
-                            child: Image.asset('assets/profile-anam.jpg'),
-                            borderRadius: BorderRadius.circular(50.0),
-                          )),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              'Role Here',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            )
-                          ],
-                        ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black26, width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: const Color(0xFFEDEDED),
                       ),
-                      Expanded(
-                        child: Text('Name here'),
-                      )
-                    ],
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black26, width: 1),
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    color: const Color(0xFFEDEDED),
-                  ),
-                )
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      child: Column(
+                        children: [
+                          Expanded(
+                              flex: 3,
+                              child: ClipRRect(
+                                child: Image.asset('assets/profile-anam.jpg'),
+                                borderRadius: BorderRadius.circular(50.0),
+                              )),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                  'Role Here',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Text('Name here'),
+                          )
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black26, width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: const Color(0xFFEDEDED),
+                      ),
+                    )
               ])),
           Expanded(
               child: Container(
