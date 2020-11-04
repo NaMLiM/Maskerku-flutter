9cbca0be820aee5c69160ff675c6d56046c7ca3a
faynald <72617106+faynald@users.noreply.github.com>
2020-11-04 22:30:45 +0700

    added many features

--- lib/page/choose_design.dart
+++ lib/page/choose_design.dart
@@ -11,7 +11,130 @@ class _ChooseDesignState extends State<ChooseDesign> {
       appBar: AppBar(
         title: Text('Choose Your Design'),
       ),
-      //drawer: DrawerCustom(),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(vertical:5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset('assets/terpopuler-1.png'),
+                              ),
+                              Text('Masker Idol')
+                            ],
+                          )
+                        ),
+                      )
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset('assets/terpopuler-2.png'),
+                              ),
+                              Text('Masker Korean Girl')
+                            ],
+                          )
+                        ),
+                      )
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset('assets/terpopuler-3.png'),
+                              ),
+                              Text('Masker Jurigen')
+                            ],
+                          )
+                        ),
+                      )
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical:5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset('assets/terpopuler-4.png'),
+                              ),
+                              Text('Masker Idol')
+                            ],
+                          )
+                        ),
+                      )
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset('assets/terpopuler-5.png'),
+                              ),
+                              Text('Masker Korean Girl')
+                            ],
+                          )
+                        ),
+                      )
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset('assets/terpopuler-3.png'),
+                              ),
+                              Text('Masker Jurigen')
+                            ],
+                          )
+                        ),
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
